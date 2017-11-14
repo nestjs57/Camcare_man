0cd46853363f402c65ae4c283509facedf428fe3
peetoey <jantaraplin_s@silpakorn.edu>
2017-11-15 03:22:31 +0700

    แก้ไขหน้า Login (ใช้ LinearLayout)

--- app/src/main/java/com/comcare/comcare_user/login.java
+++ app/src/main/java/com/comcare/comcare_user/login.java
@@ -8,6 +8,6 @@ public class login extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.item_newjob);
+        setContentView(R.layout.activity_login);
     }
 }
